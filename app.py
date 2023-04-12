1ed6b871605afbeda00af1d462b464acb9ad84b7
ilyassafi <jsafi4428@gmail.com>
2023-04-12 12:00:04 -0400

    updating the app.py so the signup and login can work with database

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 #from your_database_module import db, User
 from werkzeug.security import generate_password_hash
+from src.models import db, users
+from src.repositories.user_repository import account_repository_singleton
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 app = Flask(__name__)
 
+#  DB connection
+db_user = os.getenv('DB_USER')
+db_pass = os.getenv('DB_PASS')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+      f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('KEY')
+db.init_app(app)
 
 def hash_password(password):
     return generate_password_hash(password)
@@ -17,14 +35,22 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+
+         # Check if the username is already in use
+        existing_user = users.query.filter_by(username=username).first()
+        db_password = users.query.filter_by(password_hash=password).first()
+        if (existing_user != None) and (db_password != None):
         
-        if username == 'your_username' and password == 'your_password':
-            session['username'] = username
-            return redirect(url_for('home.html'))
+            if username == users.query.filter_by(username=username).first().username and password == users.query.filter_by(password_hash=password).first().password_hash:
+                session['username'] = username
+                return redirect(url_for('index'))
+            else:
+                return render_template('login.html', error='Password is wrong')
         else:
             return render_template('login.html', error='Invalid username or password')
 
-    return render_template('login.html')
+    else:
+        return render_template('login.html')
 
 @app.get('/signup')
 def signup():
@@ -38,31 +64,22 @@ def create_account():
         password = request.form['password']
         confirm_password = request.form['confirm_password']
 
-        # Check if the passwords match
+       # return render_template('login.html')
+       # Check if the passwords match
         if password != confirm_password:
-            return render_template('signup.html', error='Passwords do not match')
+         return render_template('signup.html', error='Passwords do not match')
 
-        # Check if the username ends with "@uncc.edu"
-        if not username.endswith('@uncc.edu'):
-            return render_template('signup.html', error='Username should end with "@uncc.edu"')
-
-        # Check if the username is already in use
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_user:
-            return render_template('signup.html', error='Username is already in use')
-
-        # Validate the password (for example, you might require a certain length or complexity)
+    # Check if the username ends with "@uncc.edu"
+        if not username.lower().endswith('@uncc.edu'):
+          return render_template('signup.html', error='Username should end with "@uncc.edu"')
+         # Validate the password (for example, you might require a certain length or complexity)
         if len(password) < 8:
             return render_template('signup.html', error='Password should be at least 8 characters long')
+        return account_repository_singleton.create_account(username.lower(), password)
 
-        # Save the user in the database
-        user = User(username=username, password=hash_password(password))
-        db.session.add(user)
-        db.session.commit()
 
-        # Set the session and redirect to the home page
-        session['username'] = username
-        return redirect(url_for('about.html'))
+        
+
 
 @app.route('/major')
 def major():

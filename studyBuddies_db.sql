8ec91fe73bf2650de96c4d7e89c7dac7dba1c9c5
jensen <jensenlobo30@outlook.com>
2023-04-12 19:08:27 -0400

    created compSci table

--- studyBuddies_db.sql
+++ studyBuddies_db.sql
@@ -1,2 +1,16 @@
 CREATE DATABASE studybuddies;
+CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password_hash VARCHAR(255) NOT NULL,
+        major VARCHAR(255)  DEFAULT 'No major',
+        first_name VARCHAR(255)  DEFAULT 'No first',
+        last_name VARCHAR(255)  DEFAULT 'No last'
+    );
 
+CREATE TABLE compSci (
+        post_id INTEGER PRIMARY KEY,
+        forum_message VARCHAR(255) NOT NULL,
+        message_likes INTEGER,
+        major_id VARCHAR(255)
+    )

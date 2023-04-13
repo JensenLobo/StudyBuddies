b4864d9d1eed937a46ffa096181fd1c3dc5a3d49
Brian Johnson <bj0020538@gmail.com>
2023-04-12 21:14:05 -0400

    Added some tables

--- studyBuddies_db.sql
+++ studyBuddies_db.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE studybuddies;
+-- This table holds all the characteristics for the user. All the user's information is stored.
 CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         username VARCHAR(255) UNIQUE NOT NULL,
@@ -8,9 +9,18 @@ CREATE TABLE users (
         last_name VARCHAR(255)  DEFAULT 'No last'
     );
 
+-- This is a table for the computer science major. 
 CREATE TABLE compSci (
         post_id INTEGER PRIMARY KEY,
         forum_message VARCHAR(255) NOT NULL,
         message_likes INTEGER,
         major_id VARCHAR(255)
-    )
+    );
+
+-- Table for general form page
+CREATE TABLE post (
+        id SERIAL PRIMARY KEY,
+        content TEXT,
+        user_id INTEGER REFERENCES users(id),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    );

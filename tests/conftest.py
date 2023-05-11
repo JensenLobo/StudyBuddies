65117a734abdb46373ceadbda94cee8ecd3c27f8
Amay Patel <amaypatel14@gmail.com>
2023-05-11 18:54:49 -0400

    adding comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 
 from app import app
 
+# This fixture is used to test the app context
 @pytest.fixture(scope='module')
 def test_app():
     with app.app_context():
--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,11 @@
 from src.models import users, db
 
+# This function is used to refresh the database
 def refresh_db():
     users.query.delete()
     db.session.commit()
 
+# This function is used to create a user for testing purposes
 def create_user(usersname='testing@uncc.edu', password_hash='abc123', user_major='no major', first_name='John', last_name='Smith', major_changed_count=0) -> users:
     test_user = users(username=usersname, password_hash=password_hash, major=user_major, first_name=first_name, last_name=last_name, major_changed_count=major_changed_count)
     db.session.add(test_user)
